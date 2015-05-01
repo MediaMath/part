e24eaf010b1913c125f478c87756caa9e581dde1
varun06 <vakumar@mediamath.com>
2015-05-01 16:57:03 -0500

    part has no make file so removing make file operations

--- .grim_build.sh
+++ .grim_build.sh
@@ -5,13 +5,9 @@ set -eu
 . /opt/golang/go1.4.2/bin/go_env.sh
 
 export GOPATH="$(pwd)/go"
-export PATH="/usr/lib/postgresql/9.1/bin:$GOPATH/bin:$PATH"
+export PATH="$GOPATH/bin:$PATH"
 
 cd "./$CLONE_PATH"
 
 go get ./...
-make clean check integration-test VERBOSE=yes
-
-if [ "$GH_EVENT_NAME" == "push" -a "$GH_TARGET" == "master" ]; then
-    REPOSITORY=libs-release-local make publish
-fi
+go test ./...
